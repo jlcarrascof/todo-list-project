3a4d3c68ca4cf59bbf4b7bc28f53602e8721b672
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-07-13 20:26:14 -0400

    Fix ESLint Errors

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,32 +1,32 @@
 class UI {
-    constructor() {
-      this.updateCheck = this.updateCheck.bind(this);
-    }
-  
+  constructor() {
+    this.updateCheck = this.updateCheck.bind(this);
+  }
+
       // displays array of lists
       displayToDo = (todo) => {
         const ul = document.getElementById('items');
         const li = document.createElement('li');
         const hr = document.createElement('hr');
         li.classList.add('flex-between');
-  
+
         li.innerHTML = `<div class="container-input">
           <input id="${todo.index}" type="checkbox" class="checkbox" ${todo.completed ? 'checked' : ''}>
           <input id="${todo.index}-inputTxt" class="todo-list-item ${todo.completed ? 'completed' : ''}" type="text" value="${todo.desc}">
         </div>
         <button class="delete" data-index="${todo.index}"><i class="fa-solid fa-trash-can" id="delete-task"></i></button>`;
-  
+
         ul.appendChild(li);
         ul.appendChild(hr);
-  
+
         this.registerEventListeners();
       };
-  
+
       clearFieldInput =() => {
         const inputList = document.getElementById('myInput');
         inputList.value = '';
       }
-  
+
       // add class completed to the checkbox input when is checked
       updateCheck = (e) => {
         const index = e.target.id;
@@ -45,7 +45,7 @@ class UI {
           localStorage.setItem('todoList', JSON.stringify(todoList));
         }
       };
-  
+
       // edits the description value with addeventlistener
         updateItem = (e) => {
           const index = e.target.id.split('-')[0];
@@ -55,19 +55,18 @@ class UI {
           todoItem.desc = value;
           localStorage.setItem('todoList', JSON.stringify(todoList));
         };
-  
+
         // event registration for checkbox and input text. Updates values
         registerEventListeners = () => {
           const checkboxes = document.querySelectorAll('.checkbox');
           checkboxes.forEach((checkbox) => {
             checkbox.addEventListener('click', this.updateCheck);
           });
-  
+
           const inputTexts = document.querySelectorAll('.todo-list-item');
           inputTexts.forEach((inputText) => {
             inputText.addEventListener('input', this.updateItem);
           });
         };
-  }
-  export default UI; 
-  
+}
+export default UI;
